4fa4134ce8bdd8d2acbeb74260be592b080b213d
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-04-07 16:46:29 -0500

    🟦 Debe_escribir_numeros_del_1_al_100 (Renombrado a Debe_generar_numeros_del_1_al_100)

--- FizzBuzz/FizzBuzz.test.cs
+++ FizzBuzz/FizzBuzz.test.cs
@@ -5,26 +5,20 @@ namespace FizzBuzz
     public class FizzBuzzTest
     {
         [Fact]
-        public void Debe_escribir_numeros_del_1_al_100()
+        public void Debe_generar_numeros_del_1_al_100()
         {
             var fizzbuzz = new FizzBuzzGenerator();
 
-            var result = fizzbuzz.Escribir();
-
+            var result = fizzbuzz.GenerarSecuencia();
 
             Assert.Equal(1, result.First());
             Assert.Equal(100, result.Last());
-
-            //Assert.Equal(100, result.Count());
-            //for (int i = 0; i < 100; i++) {
-            //    Assert.Equal(i + 1, result[i]);
-            //}
         }
     }
 
     internal class FizzBuzzGenerator
     {
-        internal List<int> Escribir()
+        internal List<int> GenerarSecuencia()
         {
             return new List<int>
             {
