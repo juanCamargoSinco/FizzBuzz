ad7ef57a4aceaa2b6631883290c0243ad9351a7b
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-04-07 16:44:00 -0500

    🟥 Debe_escribir_numeros_del_1_al_100

--- FizzBuzz/FizzBuzz.test.cs
+++ FizzBuzz/FizzBuzz.test.cs
@@ -1,11 +1,32 @@
 
+
 namespace FizzBuzz
 {
     public class FizzBuzzTest
     {
         [Fact]
-        public void Setup()
+        public void Debe_escribir_numeros_del_1_al_100()
         {
+            var fizzbuzz = new FizzBuzzGenerator();
+
+            var result = fizzbuzz.Escribir();
+
+
+            Assert.Equal(1, result.First());
+            Assert.Equal(100, result.Last());
+
+            //Assert.Equal(100, result.Count());
+            //for (int i = 0; i < 100; i++) {
+            //    Assert.Equal(i + 1, result[i]);
+            //}
+        }
+    }
+
+    internal class FizzBuzzGenerator
+    {
+        internal List<int> Escribir()
+        {
+            throw new NotImplementedException();
         }
     }
 }
